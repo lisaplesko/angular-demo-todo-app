92fdb584a3dc4da0d7feeb744efc7aa308bd7bb5
Lisa Plesko <plesko.l@gmail.com>
2014-06-18 14:34:29 -0400

    Make call to JSONstub server for todo list

--- js/app.js
+++ js/app.js
@@ -1,36 +1,24 @@
+'use strict';
+
 var todoApp = angular.module('todoApp', []);
 
-todoApp.controller('TodoController', function ($scope) {
-	$scope.todos = [
-		{
-			todoName: 'Laundry',
-			completed: false
-		},
-		{
-			todoName: 'Grocery Shopping',
-			completed: false
-		},
-		{
-			todoName: 'Wash the car',
-			completed: false
-		},
-		{
-			todoName: 'Cook dinner',
-			completed: false
-		},
-		{
-			todoName: 'Book vacay to Maui',
-			completed: false
-		},
-		{
-			todoName: 'Feed my fish',
-			completed: false
-		}
-	];
+todoApp.controller('TodoController', ['$scope', '$http', function ($scope, $http) {
+	$scope.todos = [];
+	$http({
+    url: 'http://jsonstub.com/todoapp/todos',
+    method: 'GET',
+    headers: {
+        'JsonStub-User-Key': '00369a06-656c-46a0-b167-33e47de5f50e',
+        'JsonStub-Project-Key': '3a791567-e891-4094-b975-d4f6379a7989'
+    }
+	}).success(function (todos) {
+	    $scope.todos = todos;
+	    $scope.todosCount = $scope.todos.length;
+	});
 	$scope.addNew = function(name){
 		$scope.todos.push({
 			todoName: name,
 			completed: false
 		});
 	};
-});
+}]);
